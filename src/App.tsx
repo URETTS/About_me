29d19f81abbd67c0aa28b7c337db69d69d56a947
URETTS <yurka.b2000@gmail.com>
2025-04-10 14:10:42 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-800">
+      <h1 className="text-3xl font-bold">Welcome to My Travel Blog</h1>
+    </div>
+  )
+}
+
+export default App
