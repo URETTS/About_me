3c36e8ab746fe5596ab5f9bfa991de624d729f18
URETTS <yurka.b2000@gmail.com>
2025-05-02 00:15:24 +0200

    hero_mob add

--- src/components/homePageData.tsx
+++ src/components/homePageData.tsx
@@ -1,11 +1,14 @@
 import hero from '../../assets/images/hero/hero.jpg'
+import hero_mob from '../../assets/images/hero/hero_mob.jpg'
 export interface HomePageData {
-  backgroundImage: string;
+   desktopImage: string;
+   mobileImage: string;
   welcomeMessage: Record<string, string>;
 }
 
 const homePageData: HomePageData = {
-  backgroundImage: hero,
+  desktopImage: hero,
+  mobileImage: hero_mob,
   welcomeMessage: {
     en: 'Welcome to my personal website!',
     ru: 'Добро пожаловать на мой личный сайт!',
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import homePageData from '../components/homePageData';
 import { useTranslation } from 'react-i18next';
 
@@ -6,15 +6,26 @@ const Home: React.FC = () => {
   const { i18n } = useTranslation();
   const language = i18n.language;
 
+  const [backgroundImage, setBackgroundImage] = useState(homePageData.desktopImage);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobile = window.innerWidth < 768; // Tailwind sm breakpoint
+      setBackgroundImage(isMobile ? homePageData.mobileImage : homePageData.desktopImage);
+    };
+
+    handleResize(); // init
+    window.addEventListener('resize', handleResize); // on resize
+    return () => window.removeEventListener('resize', handleResize); // cleanup
+  }, []);
+
   return (
     <section
       className="relative w-full min-h-screen bg-cover bg-center"
-      style={{ backgroundImage: `url(${homePageData.backgroundImage})` }}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      {/* Затемняющий слой */}
       <div className="absolute inset-0 bg-black/60"></div>
 
-      {/* Контент поверх фона */}
       <div className="relative z-10 flex flex-col items-center justify-center text-center min-h-screen px-4">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold uppercase tracking-widest text-white bg-black/40 px-6 py-4 rounded-xl shadow-lg animate-fadeIn">
           {homePageData.welcomeMessage[language]}
